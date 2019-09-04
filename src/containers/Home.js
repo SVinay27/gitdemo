7cb6145945e2d6381e28dc723d5aebc5bdb2d266
vinayak surywanshi <vinayaksury2012@gmail.com>
2019-09-04 18:24:10 +0530

    Home file commited

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Button>Home</Button>
+            </div>
+        )
+    }
+}
+
+export default Home
