45e4b7b9983585043a4ba9581c3dcc1a6058fa7d
vinaya surywanshi <vinayaksury2012@gmail.com>
2019-09-04 18:12:35 +0530

    sign up  commited

---
+++ src/containers/Signup.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+
+export class Signup extends Component {
+    render() {
+        return (
+            <div>
+                <Button>Sign Up</Button>
+            </div>
+        )
+    }
+}
+
+export default Signup
